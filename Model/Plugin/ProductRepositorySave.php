613f67f6da11e779f6060a80a4b5ae9af78eba1a
Yegor Shytikov <egorshitiov@gmail.com>
2021-10-13 18:58:53 -0700

    Magento Fork initial commit

--- Model/Plugin/ProductRepositorySave.php
+++ Model/Plugin/ProductRepositorySave.php
@@ -3,20 +3,21 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
+declare(strict_types=1);
+
 namespace Magento\ConfigurableProduct\Model\Plugin;
 
 use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
-use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Catalog\Model\ProductFactory;
-use Magento\ConfigurableProduct\Api\Data\OptionInterface;
-use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Framework\Exception\InputException;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\ConfigurableProduct\Api\Data\OptionInterface;
+use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Exception\LocalizedException;
 
 /**
- * Validating product links of configurable product and reset configurable attributes after save configurable product
+ * Plugin to validate product links of configurable product and reset configurable attributes
  */
 class ProductRepositorySave
 {
@@ -26,45 +27,20 @@ class ProductRepositorySave
     private $productAttributeRepository;
 
     /**
-     * @var ProductFactory
+     * @var ProductRepositoryInterface
      */
-    private $productFactory;
+    private $productRepository;
 
     /**
      * @param ProductAttributeRepositoryInterface $productAttributeRepository
-     * @param ProductFactory $productFactory
+     * @param ProductRepositoryInterface $productRepository
      */
     public function __construct(
         ProductAttributeRepositoryInterface $productAttributeRepository,
-        ProductFactory $productFactory
+        ProductRepositoryInterface $productRepository
     ) {
         $this->productAttributeRepository = $productAttributeRepository;
-        $this->productFactory = $productFactory;
-    }
-
-    /**
-     * Reset configurable attributes to configurable product
-     *
-     * @param ProductRepositoryInterface $subject
-     * @param ProductInterface $result
-     * @param ProductInterface $product
-     * @param bool $saveOptions
-     * @return ProductInterface
-     *
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function afterSave(
-        ProductRepositoryInterface $subject,
-        ProductInterface $result,
-        ProductInterface $product,
-        $saveOptions = false
-    ) {
-        if ($product->getTypeId() !== Configurable::TYPE_CODE) {
-            return $result;
-        }
-        $result->getTypeInstance()->resetConfigurableAttributes($product);
-
-        return $result;
+        $this->productRepository = $productRepository;
     }
 
     /**
@@ -83,7 +59,7 @@ class ProductRepositorySave
         ProductRepositoryInterface $subject,
         ProductInterface $product,
         $saveOptions = false
-    ) {
+    ): array {
         $result[] = $product;
         if ($product->getTypeId() !== Configurable::TYPE_CODE) {
             return $result;
@@ -109,37 +85,61 @@ class ProductRepositorySave
             $attributeCodes[] = $attributeCode;
         }
         $this->validateProductLinks($attributeCodes, $configurableLinks);
+
         return $result;
     }
 
     /**
-     * Validate required attributes and validate the same set of attribute values
+     * Reset configurable attributes to configurable product
+     *
+     * @param ProductRepositoryInterface $subject
+     * @param ProductInterface $result
+     * @param ProductInterface $product
+     * @param bool $saveOptions
+     * @return ProductInterface
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterSave(
+        ProductRepositoryInterface $subject,
+        ProductInterface $result,
+        ProductInterface $product,
+        $saveOptions = false
+    ): ProductInterface {
+        if ($product->getTypeId() !== Configurable::TYPE_CODE) {
+            return $result;
+        }
+        $result->getTypeInstance()->resetConfigurableAttributes($product);
+
+        return $result;
+    }
+
+    /**
+     * Validate product links
      *
      * @param array $attributeCodes
      * @param array $linkIds
      * @return void
      * @throws InputException
-     * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
-    private function validateProductLinks(array $attributeCodes, array $linkIds)
+    private function validateProductLinks(array $attributeCodes, array $linkIds): void
     {
         $valueMap = [];
-
         foreach ($linkIds as $productId) {
-            $variation = $this->productFactory->create()->load($productId);
+            $variation = $this->productRepository->getById($productId);
             $valueKey = '';
-            if ($variation->getId() === null) {
-                throw new LocalizedException(
-                    __('Product with id "%1" does not exist.', $productId)
-                );
-            }
             foreach ($attributeCodes as $attributeCode) {
-                if (!$variation->getData($attributeCode)) {
+                if ($variation->getData($attributeCode) === null) {
                     throw new InputException(
-                        __('Product with id "%1" does not contain required attribute "%2".', $productId, $attributeCode)
+                        __(
+                            'Product with id "%1" does not contain required attribute "%2".',
+                            $productId,
+                            $attributeCode
+                        )
                     );
                 }
-                $valueKey = $valueKey . $attributeCode . ':' . $variation->getData($attributeCode) . ';';
+                $valueKey .= $attributeCode . ':' . $variation->getData($attributeCode) . ';';
             }
             if (isset($valueMap[$valueKey])) {
                 throw new InputException(
